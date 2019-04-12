3d783ac77784d3976340e7774db5458f00a98ebd
dennistodoric <todoricdennis@gmx.de>
2019-04-12 15:09:23 +0200

    in der main methoden hinzugefügt (start() und trade()

--- src/main/scala/de/htwg/se/skyjo/Skyjo.scala
+++ src/main/scala/de/htwg/se/skyjo/Skyjo.scala
@@ -5,8 +5,13 @@ import de.htwg.se.skyjo.model.{Card, Deck, Player}
 import scala.collection.mutable
 
 object Skyjo {
-  def main(args: Array[String]): Unit = {
 
+  // TODO schleife
+  // TODO behandlung mit den hand karten weil die ja vom deck kommen müssen vorrübergehend einfach gezogen
+  // TODO x und y gleichzeitig angeben weniger readLines
+  // TODO Fehler behandlungen
+
+  def main(args: Array[String]): Unit = {
     // Ablagestapel als Stack
     val discardPile = new mutable.ArrayStack[Card]
     var deck = Deck()
@@ -17,23 +22,41 @@ object Skyjo {
     println("Projekt: Skyjo")
     println()
 
-    println(player.name)
-    println(player.printHand)
+    println(player.toString)
     println()
 
     discardPile.push(deck.drawCard())
     println("Ablagestapel: " + discardPile.top.value)
     println("Karten im Deck: " + deck.cards.length)
 
-    // TODO methoden schreiben + schleife
-    // TODO behandlung mit den hand karten weil die ja vom deck kommen müssen vorrübergehend einfach gezogen
-    // TODO x und y gleichzeitig angeben weniger readLines
-    // TODO Fehler behandlungen
+    start(player, deck)
+
+    println(player.toString)
+
+    println("Karte vom Ablagestapel nehmen? (y/n)")
+    var input = scala.io.StdIn.readLine()
+
+    if (input == "y") {
+      trade(player, discardPile.pop())
+    }else {
+      discardPile.push(deck.drawCard())
+      println("Karte " + discardPile.top.value + " gezogen")
+      trade(player, discardPile.pop())
+    }
+
+    println(player.toString)
+    println()
+    println("Ablagestapel: " + discardPile.top.value)
+    println("Karten im Deck: " + deck.cards.length)
+
+  }
+
+  def start(player: Player, deck: Deck): Unit = {
     println("Zwei Karten aufdecken.")
     println("Erste Karte y-Achse (0,1,2)")
-    y = scala.io.StdIn.readInt()
+    var y = scala.io.StdIn.readInt()
     println("Erste Karte x-Achse (0,1,2,3)")
-    x = scala.io.StdIn.readInt()
+    var x = scala.io.StdIn.readInt()
     player.hand.cards(y)(x) = deck.drawCard()
 
 
@@ -42,34 +65,15 @@ object Skyjo {
     println("Zweite Karte x-Achse (0,1,2,3)")
     x = scala.io.StdIn.readInt()
     player.hand.cards(y)(x) = deck.drawCard()
-
-    println(player.printHand)
-
-    println("Karte vom Ablagestapel nehmen? (y/n)")
-    var input = scala.io.StdIn.readLine()
-
-    if (input == "y") {
-      println("Welche Karte möchten Sie tauschen?")
-      println("y-Achse eingeben (0,1,2)")
-      y = scala.io.StdIn.readInt()
-      println("x-Achse eingeben (0,1,2,3)")
-      x = scala.io.StdIn.readInt()
-
-      player.hand.cards(y)(x) = discardPile.pop()
-    }else {
-      discardPile.push(deck.drawCard())
-      println("Karte " + discardPile.top.value + " gezogen")
-      println("Welche Karte möchten Sie tauschen?")
-      println("y-Achse eingeben (0,1,2)")
-      y = scala.io.StdIn.readInt()
-      println("x-Achse eingeben (0,1,2,3)")
-      x = scala.io.StdIn.readInt()
-
-      player.hand.cards(y)(x) = discardPile.pop()
-    }
-
-    println(player.printHand)
-    println(deck.cards.length)
-
   }
+
+  def trade(player: Player, card: Card): Unit ={
+    println("Welche Karte möchten Sie tauschen?")
+    println("y-Achse eingeben (0,1,2)")
+    var y = scala.io.StdIn.readInt()
+    println("x-Achse eingeben (0,1,2,3)")
+    var x = scala.io.StdIn.readInt()
+    player.hand.cards(y)(x) = card
+  }
+
 }
