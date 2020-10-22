4794e8687a35243a352bd9330ac72c91d2537aa7
dennistodoric <todoricdennis@gmx.de>
2020-10-22 11:16:30 +0200

    gui tui ausgestellt für wtec

--- src/main/scala/de/htwg/se/skyjo/Skyjo.scala
+++ src/main/scala/de/htwg/se/skyjo/Skyjo.scala
@@ -1,10 +1,6 @@
 package de.htwg.se.skyjo
 
-import java.io.BufferedReader
-
 import com.google.inject.{Guice, Injector}
-import de.htwg.se.skyjo.aview.Tui
-import de.htwg.se.skyjo.aview.gui.SwingGui
 import de.htwg.se.skyjo.controller.ControllerInterface
 import de.htwg.se.skyjo.controller.controllerBaseImpl.Controller
 
@@ -12,13 +8,13 @@ object Skyjo {
 
   val injector: Injector = Guice.createInjector(new SkyjoModule)
   val controller: ControllerInterface = injector.getInstance(classOf[Controller])
-  val tui = new Tui(controller)
-  val gui = new SwingGui(controller)
+  //  val tui = new Tui(controller)
+  //  val gui = new SwingGui(controller)
   controller.newGame()
 
   def main(args: Array[String]): Unit = {
     if (args.length > 0) {
-      tui.start(args(0).toInt, new BufferedReader(Console.in))
+      //      tui.start(args(0).toInt, new BufferedReader(Console.in))
       // TODO error handling
     }else {
       //tui.start(new BufferedReader(Console.in))
